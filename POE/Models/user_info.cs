54d6ad42e630d851165d66e4a47f04b57a0a0141
Owam <Owam@LAPTOP-V2J7C301>
2025-11-20 22:55:24 +0200

    Add project files.

---
+++ POE/Models/user_info.cs
@@ -0,0 +1,36 @@
+﻿namespace POE.Models
+{
+    public class user_info
+    {
+        public int Id 
+        { 
+            get;
+            set;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string Surname
+        {
+            get;
+            set;
+        }
+        public string email
+        {
+            get;
+            set;
+        }
+        public string password
+        {
+            get;
+            set;
+        }
+        public string username
+        {
+            get;
+            set;
+        }
+    }
+}
